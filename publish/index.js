09407486b7877b278fe1c9f1cef16b8d24aaad79
Tom Zhang <14881754+FlappiTomic@users.noreply.github.com>
2021-07-23 18:33:37 -0400

    publish initial commit

---
+++ publish/index.js
@@ -0,0 +1,47 @@
+/*
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright IBM Corporation 2021
+ */
+
+const core = require('@actions/core')
+const { InvalidArgumentException , utils , pax } = require('zowe-common')
+const Debug = require('debug')
+const debug = Debug('zowe-actions:shared-actions:publish')
+
+// Defaults
+const projectRootPath = process.env.GITHUB_WORKSPACE
+
+// Gets inputs
+const defaultBranchesJsonText = core.getInput('default-branches-json-text')
+const artifacts = core.getInput('artifacts')
+const performRelease = core.getInput('perform-release')
+const currentBranch = core.getInput('current-branch')
+
+var _isReleaseBranch = false
+var _isFormalReleaseBranch = false
+var _isPerformingRelease = `${ performRelease ? true : false }`
+var defaultBranchesJsonObject = JSON.parse(defaultBranchesJsonText)
+
+var match = false
+for (var i=0; i < defaultBranchesJsonObject.length; i++) {
+    var branch = defaultBranchesJsonObject[i]
+    if (currentBranch === branch.name || currentBranch.match(branch.name)) {
+        match = true
+        if (branch.hasOwnProperty('allowRelease')) {
+            _isReleaseBranch = branch.allowRelease
+        }
+        if (branch.hasOwnProperty('allowFormalRelease')) {
+            _isFormalReleaseBranch = branch.allowFormalRelease
+        }
+        break;
+    }
+}
+
+
+
+
