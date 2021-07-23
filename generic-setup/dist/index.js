9074ecb4bb01fb28f60ab059e1a8401da794aa2b
Tom Zhang <14881754+FlappiTomic@users.noreply.github.com>
2021-07-23 00:17:30 -0400

    ttt

--- generic-setup/dist/index.js
+++ generic-setup/dist/index.js
@@ -9352,6 +9352,7 @@ console.log(utils.sh('node extra-init.js && rm extra-init.js'))
 
 
 // read branches.json data into jsonObject and pass it around
+console.log(utils.sh(`ls /home/runner/work/_actions/zowe-actions/shared-actions/generic-setup`))
 var branchesJsonText = fs.readFileSync(`/home/runner/work/_actions/zowe-actions/shared-actions/generic-setup/branches.json`)
 console.log('aaaaaaaaa'+branchesJsonText)
 })();
