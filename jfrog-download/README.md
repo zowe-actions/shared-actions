5f3551d1b970a2bb2ef0b6cfcbb9b2183dc11b3b
Tom Zhang <14881754+FlappiTomic@users.noreply.github.com>
2021-10-29 17:32:41 -0400

    Initial commit of jfrog download
    Signed-off-by: Tom Zhang <14881754+FlappiTomic@users.noreply.github.com>

---
+++ jfrog-download/README.md
@@ -0,0 +1,96 @@
+# Make pax for Zowe projects
+
+This action does packaging step for Zowe projects. It transfers files over to a zOS machine to do pax then transfer files back. For more detailed process, please refer to the utility function [zowe-actions/zowe-common/lib/pax.js](https://github.com/zowe-actions/zowe-common/blob/main/lib/pax.js)
+<br />
+
+## Inputs
+
+### `pax-name`
+
+**Optional** - The name of the pax to be made. If not provided, default will be pulled from PACKAGE_INFO. Refer [here](https://github.com/zowe-actions/shared-actions/tree/main/prepare-workflow#manifest_info) for more information
+
+### `pax-ssh-host`
+
+**Optional** - The ssh zOS host for doing packaging. Default `zzow04.zowe.marist.cloud`
+
+### `pax-ssh-port`
+
+**Optional** - The ssh zOS port. Default `22`
+
+### `pax-ssh-username`
+
+**Required** - The ssh zOS login username
+
+### `pax-ssh-password`
+
+**Required** - The ssh zOS login password
+
+### `pax-local-workspace`
+
+**Optional** - The path to `.pax` folder that contains scripts required to do packaging. Default `./.pax`
+
+### `pax-remote-workspace`
+
+**Optional** - Remote directory on zOS server to be used for packaging. Default `/ZOWE/tmp`
+
+### `pax-options`
+
+**Optional** - Extra options for `pax` command
+
+### `pax-compress`
+
+**Optional** - Flag to enable pax compression
+
+### `pax-compress-options`
+
+**Optional** - Compress command options for pax
+
+### `extra-files`
+
+**Optional** - Extra artifacts will be generated and will be transferred back. For multiple items, put them on a single line with comma separation.
+
+### `keep-temp-folders`
+
+**Optional** - Flag to if we want to keep the temporary packaging folder on the remote machine for debugging purpose. Default is `FALSE`
+
+### `extra-environment-vars`
+
+**Optional** - Extra environment variables to be parsed
+<br /><br />
+
+## Outputs
+
+None
+<br /><br />
+
+## Exported environment variables
+
+None
+<br /><br />
+
+## Pre-requisite
+
+Before you call this action, make sure you call [shared-actions/prepare-workflow](https://github.com/zowe-actions/shared-actions/tree/main/prepare-workflow). Sample usage would be:
+
+```yaml
+uses: zowe-actions/shared-actions/prepare-workflow@main
+```
+
+## Example usage
+
+(this is a minimal set of inputs you need to provide)
+
+```yaml
+uses: zowe-actions/shared-actions/make-pax@main
+with:
+  pax-ssh-username: user
+  pax-ssh-password: mypassword
+```
+
+To enable debug mode, append
+
+```yaml
+env:
+  DEBUG: 'zowe-actions:zowe-common:pax'
+  DEBUG: 'zowe-actions:shared-actions:packaging'
+```
---
+++ jfrog-download/index.js
@@ -0,0 +1,171 @@
+/*
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright IBM Corporation 2021
+ */
+
+const core = require('@actions/core')
+const { utils , InvalidArgumentException } = require('zowe-common')
+const Debug = require('debug')
+const debug = Debug('zowe-actions:shared-actions:jfrog-download')
+
+// Defaults
+const projectRootPath = process.env.GITHUB_WORKSPACE
+const REPOSITORY_SNAPSHOT = 'libs-snapshot-local'
+const REPOSITORY_RELEASE = 'libs-release-local'
+
+// Gets inputs
+var manifestFilePath = core.getInput('manifest-file-path')
+var defaultTargetPath = core.getInput('default-target-path')
+var sourcePath = core.getInput('source-path')
+var defaultRepository = core.getInput('default-repository')
+var simpleDownload = false   // meaning just a simple jfrog cli command download
+var manifest2DownloadSpecDownload = false // meaning we need to convert manifest to download spec then download
+
+// mandatory check
+if (!defaultTargetPath || defaultTargetPath == '') {
+    throw new InvalidArgumentException('target-path')
+}
+if (sourcePath != '' && (!manifestFilePath || manifestFilePath == '')) {
+    simpleDownload = true
+    console.log('Simple download!')
+    console.log(`Download source is ${sourcePath}, target is ${defaultTargetPath}`)
+}
+else if (manifestFilePath != '' && (!sourcePath || sourcePath == '')) {
+    manifest2DownloadSpecDownload = true
+    console.log('Need to process manifest and generate download spec then download!')
+    console.log(`Manifest file is at ${manifestFilePath}, target is ${defaultTargetPath}`)
+}
+else { //meaning both manifest and sourcePath and provided, I am confused of what user wants to do. I give up!
+    throw new Error (```I see both sourcePath: ${sourcePath} and manifestFilePath: ${manifestFilePath} are provided.
+Do you want to simply download the ${sourcePath} or you want me to process manifest? I am confused.
+Please just have one of them, don't include both, then try again. Thanks!
+```)
+}
+
+if (manifest2DownloadSpecDownload) {
+    var downloadSpec = {"files":[]}
+    const manifestJsonObject = JSON.parse(manifestFilePath)
+    const binaryDependenciesObject = manifestJsonObject['binaryDependencies']
+    // check to make sure binaryDependencies are present
+    if (binaryDependenciesObject == '') {
+        throw new Error (`There is no binaryDependencies present in your specified manifest file: ${manifestFilePath}, this is mandatory. Try again.`)
+    }
+    for (const [packageName, definitions] of Object.entries(binaryDependenciesObject)) {
+        downloadSpec.push(processEachPackageInManifest(packageName,definitions))
+    }
+    var downloadSpecString=JSON.stringify(downloadSpec, null, 2)
+    console.log(downloadSpecString)
+}
+
+
+function processEachPackageInManifest(packageName,definitions) {
+    debug(`Processing ${packageName}:`)
+    debug(definitions)
+
+    var resultJsonObject = JSON.parse('{}')
+    var packagePath = packageName.replace(/\./g, '/')
+    var repository = ''
+
+    // if package definition has target, we will use it;
+    // if not, we will use the defaultTargetPath passed in
+    if (definitions.hasOwnProperty('target')) {
+        resultJsonObject.target = definitions.target
+    } else if (defaultTargetPath != '') {
+        resultJsonObject.target = defaultTargetPath
+    }
+
+    // if package definition has repository, we will use it;
+    //   if not, we will use the defaultRepository passed in
+    if (definitions.hasOwnProperty('repository')) {
+        repository = definitions.repository
+    } else if (defaultRepository != '') {
+        repository = defaultRepository
+    }
+
+    if (definitions.hasOwnProperty('explode')) {
+        resultJsonObject.explode = definitions.explode
+    }
+    if (definitions.hasOwnProperty('excludePatterns')) {
+        resultJsonObject.excludePatterns = definitions.excludePatterns
+    }
+    // jfrog cli shows warning of deprecation:
+    // [Warn] The --exclude-patterns command option and the 'excludePatterns' File Spec property are deprecated.
+    //         Please use the --exclusions command option or the 'exclusions' File Spec property instead.
+    //         Unlike exclude-patterns, exclusions take into account the repository as part of the pattern.
+    //         For example:
+    //         "excludePatterns": ["a.zip"]
+    //         can be translated to
+    //         "exclusions": ["repo-name/a.zip"]
+    //         or
+    //         "exclusions": ["*/a.zip"]
+    if (definitions.hasOwnProperty('exclusions')) {
+        resultJsonObject.exclusions = definitions.exclusions
+    }
+
+    // always flat
+    resultJsonObject.flat = 'true'
+
+    // now start to process pattern
+    resultJsonObject.pattern = ''
+    if (definitions.hasOwnProperty('artifact') && definitions.artifact.contains('/')) {
+        // assume this is a full path to the artifact
+        resultJsonObject.pattern = definitions.artifact
+    }
+
+    debug(`resultJsonObject (before parsing version): ${JSON.stringify(resultJsonObject, null, 2)}`)
+
+    if (!resultJsonObject.pattern && definitions.hasOwnProperty('version')) {
+        var m1 = definitions.version.match(/^~([0-9]+)\.([0-9]+)\.([0-9]+)(-.+)?$/)
+        var m2 = definitions.version.match(/^\^([0-9]+)\.([0-9]+)\.([0-9]+)(-.+)?$/)
+        if (m1) {
+            if (!repository) {
+                repository = m1[4] ? REPOSITORY_SNAPSHOT : REPOSITORY_RELEASE
+            }
+            resultJsonObject.pattern = `${repository}/${packagePath}/${m1[1]}.${m1[2]}.*${m1[4] ? m1[4] : ''}/`
+        } else if (m2) {
+            if (!repository) {
+                repository = m2[4] ? REPOSITORY_SNAPSHOT : REPOSITORY_RELEASE
+            }
+            resultJsonObject.pattern = `${repository}/${packagePath}/${m2[1]}.*${m2[4] ? m2[4] : ''}/`
+        } else {
+            // parse semantic version, this may throw exception if version is invalid
+            var semanticVersionMap = utils.parseSemanticVersion(definitions.version)
+            if (semanticVersionMap.has('prerelease') && semanticVersionMap.get('prerelease')!= '') {
+                resultJsonObject.pattern = `${repository ? repository : REPOSITORY_SNAPSHOT}/${packagePath}/${semanticVersionMap.get('major')}.${semanticVersionMap.get('minor')}.${semanticVersionMap.get('patch')}-${semanticVersionMap.get('prerelease')}/`
+            }
+            else {
+                // this is formal release
+                resultJsonObject.pattern = `${repository ? repository : REPOSITORY_RELEASE}/${packagePath}/${definitions.version}/`
+            }
+        }
+
+        if (resultJsonObject.pattern) {
+            if (definitions.hasOwnProperty('artifact')) {
+                resultJsonObject.pattern += definitions.artifact
+            } else {
+                resultJsonObject.pattern += '*'
+            }
+        }
+    }
+
+    if (!resultJsonObject.pattern) {
+        throw new InvalidArgumentException('definitions', 'Invalid artifact definitions, either artifact nor version is provided.')
+    }
+
+    if (resultJsonObject.pattern.match(/\*.*\/[^\/]+$/)) {
+        // if we have * in the path, we only pick the most recent artifact
+        // if we only have * in the artifact name, we may want to pick more than one
+        resultJsonObject.sortBy = ["created"]
+        resultJsonObject.sortOrder = "desc"
+        resultJsonObject.limit = 1
+    }
+
+    debug(`resultJsonObject (after parsing version): ${JSON.stringify(resultJsonObject, null, 2)}`)
+
+    return resultJsonObject
+}
