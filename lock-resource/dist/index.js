c806077f8296e20d055239f2dec285a57ac28474
Tom Zhang <tom.yczhang@gmail.com>
2022-01-13 17:58:36 -0500

    Change to use a different sh execute method to see live status
    Signed-off-by: Tom Zhang <tom.yczhang@gmail.com>

--- lock-resource/dist/index.js
+++ lock-resource/dist/index.js
@@ -13205,6 +13205,7 @@ if (!core.getState('isLockPost')) {
     myLockJson.actor = context.actor
     myLockJson.runId = context.runId
     myLockJson.runNumber = context.runNumber
+    myLockJson.link = `${context.serverUrl}/${context.repo.owner}/${context.repo.repo}/actions/runs/${context.runId}`
     
     github.shallowClone(lockRepository,lockRoot,lockBranch,true)
     lock(myLockJson)
