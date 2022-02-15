d5313af06b2fd2e3e3c25d1502276c6c0f887f36
Tom Zhang <tom.yczhang@gmail.com>
2022-02-15 15:08:41 -0500

    Update index.js
    Signed-off-by: Tom Zhang <tom.yczhang@gmail.com>

--- workflow-remote-call-wait/index.js
+++ workflow-remote-call-wait/index.js
@@ -5,7 +5,7 @@
  *
  * SPDX-License-Identifier: EPL-2.0
  *
- * Copyright IBM Corporation 2021
+ * Copyright IBM Corporation 2022
  */
 
 const core = require('@actions/core')
@@ -31,6 +31,8 @@ utils.mandatoryInputCheck(workflowFileName,'workflow-filename')
 utils.mandatoryInputCheck(branchName,'branch-name')
 utils.mandatoryInputCheck(pollFrequency,'poll-frequency')
 
+// ======= send dispatch POST event =========
+
 // insert a random dispatch event id into the json inputs
 var inputsJsonObject = JSON.parse(inputsJsonString)
 inputsJsonObject['RANDOM_DISPATCH_EVENT_ID'] = Math.random().toString(36).substring(2) //get a random alphanumeric ID
@@ -46,7 +48,40 @@ cmdDispatchEvent.push(`"https://api.github.com/repos/${owner}/${repo}/actions/wo
 cmdDispatchEvent.push(`-d '{"ref":"${branchName}","inputs":${inputsJsonStringFinal}}'`)
 
 var out = utils.sh(cmdDispatchEvent.join(' '))
+// this POST request does not have anything returned upon success
 if (out != '') {
     console.error(out)
     throw new Error('Workflow dispatch event POST request failed')
-} //this POST request does not have anything returned upon success
+} 
+
+await utils.sleep(2*1000) // sleep for 2 seconds to give enough time to have the new job show up
+
+// ====== get back a list of workflow runs ======
+
+var cmdGetWorkflowRun = new Array()
+cmdGetWorkflowRun.push(`curl`)
+cmdGetWorkflowRun.push(`-H "Accept: application/vnd.github.v3+json"`)
+cmdGetWorkflowRun.push(`-H "Authorization: Bearer ${githubToken}"`)
+cmdGetWorkflowRun.push(`-X GET`)
+cmdGetWorkflowRun.push(`"https://api.github.com/repos/${owner}/${repo}/actions/workflows/${workflowFileName}/runs"`)
+cmdGetWorkflowRun.push(`| jq -r ".workflow_runs[:10]"`)  // only get the first ten workflow runs returned
+
+var getWFRunOut = utils.sh(cmdGetWorkflowRun.join(' ')) //expect a json array coming out
+var workflowRunListJsonObject = JSON.parse(getWFRunOut)
+var runNumber
+
+// this grand loop searches for the workflow that is triggered by this action
+workflowRunListJsonObject.forEach(function(eachWFRun) { 
+    // when the job status is 'queued', no job information is available
+    // thus we must wait until the job status becomes either 'in_progress' or 'completed'
+    // also ignore anything that is not workflow_dispatch because apparently that is not triggered by this action
+    if (eachWFRun['event'] == 'workflow_dispatch' && eachWFRun['status'] != 'queued') {
+
+    }
+    var jobsURL = eachWFRun['jobs_url']
+    var status = 
+    runNumber = eachWFRun['run_number']
+
+
+});
+
